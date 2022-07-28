e89669fe865c00debb4febab6f66c85d09021454
Nicholas Nemtsev <knemtsev@gmail.com>
2022-07-28 19:52:17 +0300

    chinese language

--- locale/init.sql
+++ locale/init.sql
@@ -5,3 +5,5 @@
 INSERT INTO db.locale (id, code, name, description) VALUES ('00000000-0000-4001-a000-000000000001', 'en', 'English', 'English - United States');
 INSERT INTO db.locale (id, code, name, description) VALUES ('00000000-0000-4001-a000-000000000002', 'ru', 'Русский', 'Русский язык');
 
+INSERT INTO db.locale (id, code, name, description) VALUES ('00000000-0000-4001-a000-000000000003', 'zh', 'Chinese', 'Chinese language');
+
